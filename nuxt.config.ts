ed11967d235a76570b90338af732dc0ac8ae2803
Manuchehr <manuchekhr.3232@gmail.com>
2023-02-19 12:43:34 +0500

    feat: add eslint, prettier & basic configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  ssr: true,
+  app: {
+    head: {
+      title: 'Nuxt3 project',
+    },
+  },
+  nitro: {
+    serveStatic: true,
+  },
+  devServerHandlers: [],
+  runtimeConfig: {
+    public: {
+      baseURL: 'localhost',
+    },
+  },
 })
